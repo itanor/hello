f2fbded9517118603a76e1911830ef132a6e9fb0
Itanor Strapazzon <itanor@gmail.com>
2021-10-21 21:43:50 -0300

    refactor: Area as a Rectangle method

--- structsmethodsinterfaces/shapes_test.go
+++ structsmethodsinterfaces/shapes_test.go
@@ -17,7 +17,7 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
   t.Run("rectangles", func(t *testing.T) {
     rectangle := Rectangle{12.0, 6.0}
-    got := Area(rectangle)
+    got := rectangle.Area()
     want := 72.0
 
     if got != want {
