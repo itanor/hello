a1b1f316c679f168265a9a7235fb77aba8b98a32
Itanor Strapazzon <itanor@gmail.com>
2022-02-08 23:17:55 -0300

    fix dictionary var name

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,17 +5,17 @@ import (
 )
 
 func TestSearch(t *testing.T) {
-  dictionay := Dictionary{"test": "this is just a test"}
+  dictionary := Dictionary{"test": "this is just a test"}
 
   t.Run("know word", func(t *testing.T) {
-    got, _ := dictionay.Search("test")
+    got, _ := dictionary.Search("test")
     want := "this is just a test"
 
     assertStrings(t, got, want)
   })
 
   t.Run("unknow word", func(t *testing.T) {
-    _, got := dictionay.Search("unknown")
+    _, got := dictionary.Search("unknown")
 
     assertError(t, got, ErrNotFound)
   })
@@ -40,23 +40,23 @@ func assertError(t testing.TB, got, want error) {
 func TestAdd(t *testing.T) {
 
   t.Run("new word", func(t *testing.T) {
-    dictionay := Dictionary{}
+    dictionary := Dictionary{}
     key := "test"
     value := "this is just a test"
-    err := dictionay.Add(key, value)
+    err := dictionary.Add(key, value)
 
     assertError(t, err, nil)
-    assertValue(t, dictionay, key, value)
+    assertValue(t, dictionary, key, value)
   })
 
   t.Run("existing key", func(t *testing.T) {
     key := "test"
     value := "this is just a test"
-    dictionay := Dictionary{key: value}
-    err := dictionay.Add(key, "new test")
+    dictionary := Dictionary{key: value}
+    err := dictionary.Add(key, "new test")
 
     assertError(t, err, ErrKeyExists)
-    assertValue(t, dictionay, key, value)
+    assertValue(t, dictionary, key, value)
   })
 }
 
@@ -65,21 +65,21 @@ func TestUpdate(t *testing.T) {
   t.Run("existing key", func(t *testing.T) {
     key := "test"
     value := "this is just a test"
-    dictionay := Dictionary{key: value}
+    dictionary := Dictionary{key: value}
     newValue := "new value"
 
-    err := dictionay.Update(key, newValue)
+    err := dictionary.Update(key, newValue)
 
     assertError(t, err, nil)
-    assertValue(t, dictionay, key, newValue)
+    assertValue(t, dictionary, key, newValue)
   })
 
   t.Run("new key", func(t *testing.T) {
     key := "test"
     value := "this is just a test"
-    dictionay := Dictionary{}
+    dictionary := Dictionary{}
 
-    err := dictionay.Update(key, value)
+    err := dictionary.Update(key, value)
 
     assertError(t, err, ErrKeyNotExists)
   })
@@ -88,20 +88,20 @@ func TestUpdate(t *testing.T) {
 func TestDelete(t *testing.T) {
   key := "test"
   value := "some value"
-  dictionay := Dictionary{key: value}
+  dictionary := Dictionary{key: value}
 
-  dictionay.Delete(key)
+  dictionary.Delete(key)
 
-  _, err := dictionay.Search(key)
+  _, err := dictionary.Search(key)
   if err != ErrNotFound {
     t.Errorf("Expected %q to be deleted", key)
   }
 }
 
-func assertValue(t testing.TB, dictionay Dictionary, key, value string) {
+func assertValue(t testing.TB, dictionary Dictionary, key, value string) {
   t.Helper()
 
-  got, err := dictionay.Search(key)
+  got, err := dictionary.Search(key)
   if err != nil {
     t.Fatal("should find added value", err)
   }
